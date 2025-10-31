f47d942add67d3415e170fec85525dffb1c83753
leebape92 <leebape92@gmail.com>
2025-10-31 23:38:38 +0900

    예약 패키지 기본 구성

---
+++ src/main/java/com/example/demo/reservation/dto/ReservationDTO.java
@@ -0,0 +1,5 @@
+package com.example.demo.reservation.dto;
+
+public class ReservationDTO {
+
+}
---
+++ src/main/java/com/example/demo/reservation/entity/ReservationEntity.java
@@ -0,0 +1,5 @@
+package com.example.demo.reservation.entity;
+
+public class ReservationEntity {
+
+}
